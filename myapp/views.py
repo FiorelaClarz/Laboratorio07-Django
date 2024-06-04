848a12201c3e0f997457f6394c9f857ff47d094a
Fiorela <clafiorelariza@gmail.com>
2024-06-04 13:35:18 -0500

    poniendo la capeta images de static como baseUrl

--- myapp/views.py
+++ myapp/views.py
@@ -17,19 +17,19 @@ def add(request):
 def home(request):
 
     dest1=Destination()
-    dest1.name="Mumbai"
+    dest1.name="Arequipa"
     dest1.desc="The city that never sleeps"
     dest1.img="destination_1.jpg"
     dest1.price=700
 
     dest2=Destination()
-    dest2.name="Hiderabab"
+    dest2.name="Lima"
     dest2.desc="Not recomendable"
     dest2.img="destination_2.jpg"
     dest2.price=400
 
     dest3=Destination()
-    dest3.name="Bengaluru"
+    dest3.name="Tacna"
     dest3.desc="Beautiful City"
     dest3.img="destination_3.jpg"
     dest3.price=800    
