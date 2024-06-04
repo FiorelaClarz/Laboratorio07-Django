ac98087ae654c5b29557845703e6bcf6339cb081
Fiorela <clafiorelariza@gmail.com>
2024-06-04 13:39:23 -0500

    añadiendo en views.py --> Destination() ofertas

--- myapp/views.py
+++ myapp/views.py
@@ -21,18 +21,21 @@ def home(request):
     dest1.desc="The city that never sleeps"
     dest1.img="destination_1.jpg"
     dest1.price=700
+    dest1.offer= True
 
     dest2=Destination()
     dest2.name="Lima"
     dest2.desc="Not recomendable"
     dest2.img="destination_2.jpg"
     dest2.price=400
+    dest2.offer= False
 
     dest3=Destination()
     dest3.name="Tacna"
     dest3.desc="Beautiful City"
     dest3.img="destination_3.jpg"
     dest3.price=800    
+    dest3.offer= True
 
     dests=[dest1,dest2,dest3]
 
